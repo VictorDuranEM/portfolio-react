6fce83419ff3eb3c0a9bbff49bbaa4f64f4e20ed
Victor Duran <victor.duran.em@gmail.com>
2022-10-24 15:21:13 -0500

    Fix focus style in frontend mentor icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <a className="text-h3 hover:opacity-70 hover:underline font-black break-words" href="mailto:victor.duran.em@gmail.com">victor.duran.em@gmail.com</a>
       <ul className="flex justify-center items-center mt-8 gap-8">
         <li><a className="text-h3 hover:opacity-70" href="https://github.com/VictorDuranEM" target='_blank'><i className="fa-brands fa-github"></i></a></li>
-        <li><a className="text-h3 hover:opacity-70" href="https://www.frontendmentor.io/profile/VictorDuranEM" target='_blank'><img className='w-[1.7rem]' src={frontendMentorLogo}/></a></li>
+        <li><a className="text-h3 hover:opacity-70 inline-block" href="https://www.frontendmentor.io/profile/VictorDuranEM" target='_blank'><img className='w-[1.7rem]' src={frontendMentorLogo}/></a></li>
         <li><a className="text-h3 hover:opacity-70" href="https://twitter.com/EnterMatchPulse" target='_blank'><i className="fa-brands fa-twitter"></i></a></li>
       </ul>
     </footer>
