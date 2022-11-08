6c15e25e3260c051270caf6fad153a4e092216f0
Victor Duran <victor.duran.em@gmail.com>
2022-11-07 23:52:41 -0500

    Add sigle page design project

--- src/assets/projects.tsx
+++ src/assets/projects.tsx
@@ -3,6 +3,7 @@ import eLearningCard from './images/work/e-learning-card.png';
 import equalizerCard from './images/work/equalizer-card.png';
 import meetCard from './images/work/meet-card.png';
 import podRequestAccessCard from './images/work/pod-request-access-card.png';
+import singlePageDesignCard from './images/work/single-page-design-card.png';
 
 export type Project = {
   id: string,
@@ -142,4 +143,40 @@ export const projects: Project[] = [
       },
     ],
   },
+  {
+    id: '5',
+    title: 'Single Page Design',
+    titleStrong: 'Landing Page',
+    subtitle: 'Challenge',
+    image: singlePageDesignCard,
+    description: 'This landing page is my implementation of a challenge from Frontend Mentor.',
+    technologies: [
+      {
+        name: 'Next.js',
+        url: 'https://nextjs.org/'
+      },
+      {
+        name: 'React',
+        url: 'https://reactjs.org/'
+      },
+      {
+        name: 'Tailwind CSS',
+        url: 'https://tailwindcss.com/'
+      },
+      {
+        name: 'CVA',
+        url: 'https://github.com/joe-bell/cva'
+      }
+    ],
+    links: [
+      {
+        name: 'View on Vercel',
+        url: 'https://single-page-design-portfolio-peach.vercel.app/'
+      },
+      {
+        name: 'View on GitHub',
+        url: 'https://github.com/VictorDuranEM/single-page-design-portfolio'
+      },
+    ],
+  }
 ]
